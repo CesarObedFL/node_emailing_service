c799ca1aa669f6518e18e91ad4cd8a051e8ca872
CesarObedFL <cesarobedfl@gmail.com>
2025-01-11 12:39:12 -0600

    [ADD] /send-email endpoint: draft

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json()); // for parsing application/json
+
+
+app.post("/send-email", (req, res, next) => {
+    const message = req.body.message;
+    res.json({"receivedMessage": message});
+});
+
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+
    
