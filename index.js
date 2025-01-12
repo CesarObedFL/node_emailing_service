20fd4af6ead8311a29110d46a821e209f0ca982b
CesarObedFL <cesarobedfl@gmail.com>
2025-01-12 11:02:46 -0600

    [ADD] nodemailer transporter

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
 const express = require('express');
+const nodemailer = require('nodemailer');
 const app = express();
+
+const email = process.env.EMAIL;
+const email_password = process.env.EMAIL_PASSWORD;
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail', // Use your email provider
+    auth: {
+        user: email,
+        pass: email_password,
+    }
+});
+
 
 app.use(express.json()); // for parsing application/json
 
