a20d16a83f82be4e5117b8c3fa8f36530c1d4786
CesarObedFL <cesarobedfl@gmail.com>
2025-10-13 04:46:20 -0600

    [ADD] GOOGLE_APP_CREDENTIALS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,10 @@ module.exports = {
             name: "node_emailing_service",
             script: "./index.js",
             watch: true,
+
+            env_production: {
+                GOOGLE_APPLICATION_CREDENTIALS: "/var/www/node_emailing_service/config/portfolio-1737941968535-64bfb8ff049b.json" 
+            }
         }
     ]
 }
