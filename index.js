213bc835cb258d1a97f2e800a6e83df5de7aedb0
CesarObedFL <cesarobedfl@gmail.com>
2026-08-18 06:45:36 -0600

    [RM] index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./server');
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`🚀 Email microservice running on port ${port}`);
-    console.log(`🔗 CORS allowed: ${process.env.CLIENT_URL || 'undefined'}`);
-});
